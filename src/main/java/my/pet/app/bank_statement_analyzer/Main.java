a6ed23c84328989235eab01252c20eb76c34d5b0
Vladimir Baksheev <vladimir.baksheev@gmail.com>
2024-03-02 16:45:30 +0500

    Добавил вывод общей суммы всемх операций
    На данном этапе использовано наипростейшее решение - просто считать содержимое файла
    и посчитать сумму всех операций. Весь код расположен в Main

--- src/main/java/my/pet/app/bank_statement_analyzer/Main.java
+++ src/main/java/my/pet/app/bank_statement_analyzer/Main.java
@@ -1,7 +1,25 @@
 package my.pet.app.bank_statement_analyzer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(String[] args) throws IOException {
+        final Path path = Paths.get(RESOURCES + args[0]);
+        final List<String> lines = Files.readAllLines(path);
+        double total = 0;
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final double amount = Double.parseDouble(columns[1]);
+            total += amount;
+        }
+
+        System.out.println("The total for all transactions: " + total);
     }
 }
